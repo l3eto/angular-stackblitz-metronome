15dbd4f8d67363d0ada6588616deb07ccb3d2cb4
l3eto <beto_2613@hotmail.com>
2020-03-03 09:52:49 +0100

    update1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MetronomeComponent } from './metronome.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, MetronomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
