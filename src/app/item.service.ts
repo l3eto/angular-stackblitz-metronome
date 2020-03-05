c01de589ca5693e92f39a821a0bcaa24d0761d0b
l3eto <beto_2613@hotmail.com>
2020-03-05 13:34:05 +0100

    2 files modified

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -6,9 +6,8 @@ import { HttpClient } from '@angular/common/http';
 export class ItemService {
   constructor(private http: HttpClient) { }
   url = 'http://www.mocky.io/v2';
-  getCharacters() {
-    return this
-            .http
-            .get(`${this.url}/5e60ddce3300005fcc97bce5`);
+
+  getItems() {
+    return this.http.get(`${this.url}/5e60ddce3300005fcc97bce5`);
   }
 }
--- src/app/tab1/tab1.component.ts
+++ src/app/tab1/tab1.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ItemService }  from './../item.service';
+import { Item } from './../item';
 
 @Component({
   selector: 'tab1',
   templateUrl: './tab1.component.html',
   /*styles: [`h1 { font-family: Lato; }`]*/
 })
-export class Tab1Component  {
+export class Tab1Component  implements OnInit {
   /*@Input() name: string;*/
+
+  constructor(private itemService: ItemService) {}
+  
+  ngOnInit() {
+    this.itemService.getItems()
+      .subscribe((data: Item) => this.items = {
+          heroesUrl: data['heroesUrl'],
+          textfile:  data['textfile']
+      });
+  }
+
 }
