32f28bb5f2f32f29aacf6bc8f068d56e55bfaaf8
l3eto <beto_2613@hotmail.com>
2020-03-06 12:29:48 +0100

    bootstrap added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
  // categories: any[] = categories;
- categories: any[] = [];
+ categories: any[] = [
+   {name: 'Items', href: 'items'}
+ ];
   isCollapsed = false;
   constructor() { }
 
