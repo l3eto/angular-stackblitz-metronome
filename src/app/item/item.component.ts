9e65aaf1d1245f9b0d9fc92d40f5309e9fcf8ace
l3eto <beto_2613@hotmail.com>
2020-03-13 12:14:21 +0100

    4 files modified, 1 added

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ItemService }  from './../item.service';
 import { Item } from './../item';
 import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'items',
   templateUrl: './item.component.html',
-  /*styles: [`h1 { font-family: Lato; }`]*/
+  styleUrls: ['./item.component.css']
 })
 
 export class ItemComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private itemService: ItemService) {}
   item: Item;isLoadingResults = true;
+  constructor(private route: ActivatedRoute, private itemService: ItemService) {}
   
   ngOnInit() {
-    this.itemService.getItem("").subscribe(data => this.item = data.item);
+    this.itemService.getItem(this.route.snapshot.params.id).subscribe((data: any) => {
+      this.item = data.item;
+      this.isLoadingResults = false;
+    });
   }
 
 }
