a511e47cec8406f71e370ea708fdfef5ba5fa373
l3eto <beto_2613@hotmail.com>
2020-03-13 12:07:19 +0100

    1 file modified

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService }  from './../item.service';
 import { Item } from './../item';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'items',
   templateUrl: './item.component.html',
@@ -10,8 +10,8 @@ import { Item } from './../item';
 
 export class ItemComponent implements OnInit {
 
-  constructor(private itemService: ItemService) {}
-  item: Item;
+  constructor(private route: ActivatedRoute, private itemService: ItemService) {}
+  item: Item;isLoadingResults = true;
   
   ngOnInit() {
     this.itemService.getItem("").subscribe(data => this.item = data.item);
